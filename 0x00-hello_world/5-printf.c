ace0d1dd535a4b5aca281d1fd60d125a910d7258
Manasseh Amoadu <mamoadu@gmail.com>
2023-03-16 23:43:52 +0000

    a c program that prints using function printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Description: main - A program that prints line using printf function
+ * Return: 0 if success
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is apiece of art,\n");
+return (0);
+}
