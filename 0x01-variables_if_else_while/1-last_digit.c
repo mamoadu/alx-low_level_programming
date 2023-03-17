92cef7afed5f091b360f09da28dffccb5f1e5abd
Manasseh Amoadu <mamoadu@gmail.com>
2023-03-17 23:15:47 +0000

    prints last digit of a number stored in a variable n

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ int main(void)
 	}
 	if ((n % 10) == 6)
 	{
-		printf("Last digit of %d is %d and is 0\n", n,- n % 10);
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	}
 	if ((n % 10) < 6 && (n % 10) != 0)
 	{
