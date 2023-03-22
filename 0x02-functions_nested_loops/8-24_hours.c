00ec49b7108869690930777da4e229358ca2998f
Manasseh Amoadu <mamoadu@gmail.com>
2023-03-22 00:10:41 +0000

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,10 +14,10 @@ void jack_bauer(void)
 		j = 0;
 		while (j < 60)
 		{
-			_putchar((i /10) + '0');
+			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
 			_putchar(':');
-			_putchar((j /10) + '0');
+			_putchar((j / 10) + '0');
 			_putchar((j % 10) + '0');
 			_putchar('\n');
 			j++;
