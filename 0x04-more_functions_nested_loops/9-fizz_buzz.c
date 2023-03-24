21982f537dd0bfdc83624537532316a76609b813
Manasseh Amoadu <mamoadu@gmail.com>
2023-03-24 23:36:13 +0000

    Take Task 9. Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - fizz-buzz test
+ * Return: 0
+ */
+int main(void)
+{
+	int m;
+
+	m = 1;
+	printf("%d", m);
+
+	for (m = 2; m <= 100; m++)
+	{
+		if ((m % 3 == 0) && (m % 5 == 0))
+		{
+			printf(" FizzBuzz");
+		}
+		else if (n % 3 == 0)
+		{
+			printf(" Fizz");
+		}
+		else if (m % 5 == 0)
+		{
+			printf(" Buzz");
+		}
+		else
+		{
+			printf(" %d", m);
+		}
+	}
+	printf("\n");
+	return (0);
+}
