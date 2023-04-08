6ad6e24d448305b8c82800c38c41c3a73a8ebc16
Manasseh Amoadu <mamoadu@gmail.com>
2023-04-08 22:06:41 +0000

    Task 6 function that prints the chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_chessboard - entry point
+ * @a: array
+ * Return: 0
+ */
+void print_chessboard(char (*a)[8])
+{
+	int m;
+	int n;
+
+	for (m = 0; m < 8; m++)
+	{
+		for (n = ; n < 8; n++)
+			_putchar(a[m][n]);
+		_putchar('\n');
+	}
+}
