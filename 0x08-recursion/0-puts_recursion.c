581132682af8f472bc4d275db7d268410a43b5c3
Manasseh Amoadu <mamoadu@gmail.com>
2023-04-09 16:42:42 +0000

    function that prints a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts_recursion - function that prints a string
+ * @s: input value
+ * Return: (0)
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	_putchar('\n');
+}
