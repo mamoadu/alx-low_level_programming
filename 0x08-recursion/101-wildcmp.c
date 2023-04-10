ca5c9900f0a27e5ca2605bc8462de447550afe90
Manasseh Amoadu <mamoadu@gmail.com>
2023-04-10 12:23:32 +0000

    function that compares two strings and returns 1, if identical, otherwise return 0

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * wildcmp - function that compare two strings
+ * @s1: input string one
+ * @s2: input string two
+ * Return: 1 if strings are identical, 0 otherwise
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
